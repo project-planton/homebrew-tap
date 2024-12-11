2306e0a0144dd509b4ab9667e97c0d48132f776c
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-12-11 16:52:14 +0530

    releasing version v0.0.34 that uses pulumi-modules from the consolidated mono-repo

--- Formula/project-planton.rb
+++ Formula/project-planton.rb
@@ -1,5 +1,5 @@
 class ProjectPlanton < Formula
-  version "v0.0.19"
+  version "v0.0.34"
   desc "ProjectPlanton: OpenSource Multi-Cloud Deployment Framework"
   homepage "https://project-planton.org"
   os_arch = `arch`
